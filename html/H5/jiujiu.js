43a48002d3d13a7d3727d1c8dbbc36514b739979
craftlkhugetree <craftlk@outlook.com>
2020-07-13 17:55:51 +0800

    H5

---
+++ html/H5/jiujiu.js
@@ -0,0 +1,13 @@
+function jiujiu(){
+    var b99 = []
+    for(var i=1;i<10;i++){
+        for(var j=1;j<=i;j++){
+            // console.log(`${j}*${i}=${j*i};`)
+            var a99 = document.getElementsByTagName("li")
+            console.log(a99)
+            a99.innerHTML =  i + '*' + j + '=' + i * j+ "&nbsp&nbsp";
+        }
+        // console.log('\n')
+    }
+}
+
