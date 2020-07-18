593a0471ebe01f57590b463e5d91146a7be1b13e
craftlkhugetree <craftlk@outlook.com>
2020-07-18 17:54:58 +0800

    dikuaizhuanxiang

--- angular/angulardemo03/src/app/components/home/home.component.ts
+++ angular/angulardemo03/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient, HttpRequest, HttpResponse } from '@angular/common/http';
+import { NzTableModule } from 'ng-zorro-antd/table';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,25 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.initdata()
+  }
+
+  searchObj = {
+    // 组织ID
+    cOrgUUID: this.current_org.cOrgUUID,
+    // 帐套ID
+    cAccUniqueID: this.account.cAccUniqueID,
+  };
+  landEvaluatingTable:any;
+  initdata() {
+    this.http.post('/cnic-dcas/AnalysisBySynthesisAction/getSpatialDataOverlay', this.searchObj)
+    .subscribe((res: any) => {
+      if (res.code === 1) {
+        this.landEvaluatingTable = res.data;
+      } else {
+        console.log('error'); 
+      }
+    });
   }
 
 }
