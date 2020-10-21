a147d0289b50aee3d1b1eda4aed596781f9794ec
craftlkhugetree <craftlk@outlook.com>
2020-10-21 11:36:16 +0800

    fix:monthly-report:dateFloat

---
+++ java/java单体.js
@@ -0,0 +1,28 @@
+https://www.bilibili.com/video/av29299488
+千锋教育  李卫民
+
+1.有道无术，术尚可求。有术无道，止于术。
+批判性思维：对思考的思考。
+
+2.接口处理各个对象间的协作关系，将“定义”与“实现”分离，实现系统解耦的目的。
+面向接口编程，先有接口再有实现，即
+public interface AnimalService{}  // first
+public class CatServiceImpl implements AnimalService{}  // then
+
+3.面向对象设计原则为我们提供了方法和准则：开口合里最单依。
+开：面向扩展开放，面向修改关闭；（其它六种准则是对“开”的维护）
+口：接口隔离原则；
+合：组合/聚合原则；
+里：里氏替换
+最：最少知识
+单：单一职责
+依：依赖倒置
+
+4.看山是山本身是现象：学会借用，ICQ（国外）——OICQ——QQ；
+看山不是山是本质：学会遗忘，微信在设计之初是一款高速邮箱软件；
+看山还是山是现象与本质的统一。
+
+5.MyEclipse和Spring STS都是Eclipse的插件，前者收费。
+IntelliJ IDEA 是JetBrains的产品。
+
+6.快捷方式psvm，sout
